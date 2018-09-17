35f9c7fdcded215c9f86f5d7c5cba1ed1937bf83
U-BSD\323382 <323382@SHS-10L74C39A.beaverton.k12.or.us>
2018-09-17 09:15:15 -0700

    added pass 2D array by pointer

--- tictactoe/tictactoe.cpp
+++ tictactoe/tictactoe.cpp
@@ -2,26 +2,47 @@
 
 using namespace std;
 
-void printBoard(int board[3][3]);
-void clearBoard(int &board[3][3]);
-int turn(int &board[3][3]);
-void getMove(int &move[2]);
-int checkWin(int board[3][3]);
+void printBoard(int** board);
+void clearBoard(int** board);
+int turn(int** board, int player);
+void getMove(int* move, int player);
+int checkWin(int** board);
+const int X = 1;
+const int O = 2;
 
 main(){
-  int board[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-  clearBoard(&board);
-  printBoard(board);
+  int** board;
+  board = new int*[3];
+  for (int i = 0; i < 3; i++){
+    board[i] = new int[3];
+  }
+
+  int* move = new int[3];
   
+  clearBoard(board);
+  printBoard(board);
 }
 
-void printBoard(int board[3][3]){
-  cout << " a b c" << endl;
+void printBoard(int** board){
+  cout << "  a b c" << endl;
   for (int i = 0; i < 3; i++){
     cout << i + 1 << " " << board[i][0] << " " << board [i][1] << " " << board[i][2] << endl;
   }
 }
 
-void clearBoard(int &board[3][3]){
-  board = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+void clearBoard(int** board){
+  for (int i = 0; i < 3; i++){
+    for (int j = 0; j < 3; j++){
+      board[i][j] = 0;
+    }
+  }
+}
+
+int turn(int** board, int player){
+  
+  
+}
+
+void getMove(int* move, int player){
+  cout << "Player " << player << "enter a move: " << endl;
 }
